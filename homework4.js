5b8330df41744fafd48dce6fb262edd1144ed05a
po-hsiang666 <zooz861031@gmail.com>
2021-06-27 01:53:04 +0800

    20210627

--- homework4.js
+++ homework4.js
@@ -25,8 +25,6 @@ class vector
 }
 var v1 = new vector(1,2,3);
 var v2 = new vector (4,5,6,);
-console.log("\nv1=%s",v1);
-console.log("v2=%s",v2);
 console.log("\nv1 + v2 = %s",v1.add(v2));//顯示加法結果
 console.log("\nv1 - v2 = %s",v1.sub(v2));//顯示減法結果
 console.log("\nv1 ‧  v2 = %s",v1.dot(v2));//顯示內積結果
